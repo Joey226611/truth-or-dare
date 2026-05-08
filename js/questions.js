dfc30f9ec46a5473551b4f5787f18ec98fe36a08
Joey226611 <157316686+Joey226611@users.noreply.github.com>
2026-05-08 22:12:54 +0200

    Add truthQuestions array to questions.js

---
+++ js/questions.js
@@ -0,0 +1,10 @@
+export const truthQuestions = [
+  "Wat is je grootste geheim?",
+  "Op wie heb je een crush?",
+  "Wat is het meest gênante dat je ooit deed?",
+  "Wat is je grootste angst?",
+  "Wanneer heb je voor het laatst gelogen?",
+  "Wat is je slechtste gewoonte?",
+  "Wat is je meest awkward moment?",
+  "Wie vind je het leukst in deze lobby?"
+];
