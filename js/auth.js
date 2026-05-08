712db42ca31adadb2e1937568c3ce487ce2170a4
Joey226611 <157316686+Joey226611@users.noreply.github.com>
2026-05-08 22:29:21 +0200

    Refactor user registration and login to use username

--- js/auth.js
+++ js/auth.js
@@ -4,24 +4,33 @@ import {
   signInWithEmailAndPassword
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
-const email = document.getElementById("email");
+const username = document.getElementById("email");
 const password = document.getElementById("password");
 const status = document.getElementById("status");
 
+// echte email maken van username
+function makeEmail(name){
+  return name.replace(/\s/g,"").toLowerCase() + "@doenofwaarheid.app";
+}
+
 document.getElementById("register").onclick = async () => {
   try {
-    await createUserWithEmailAndPassword(auth, email.value+"@game.nl", password.value);
+    const email = makeEmail(username.value);
+    await createUserWithEmailAndPassword(auth, email, password.value);
     location.href = "lobby.html";
   } catch(e) {
     status.innerText = e.message;
+    console.error(e);
   }
 };
 
 document.getElementById("login").onclick = async () => {
   try {
-    await signInWithEmailAndPassword(auth, email.value+"@game.nl", password.value);
+    const email = makeEmail(username.value);
+    await signInWithEmailAndPassword(auth, email, password.value);
     location.href = "lobby.html";
   } catch(e) {
     status.innerText = e.message;
+    console.error(e);
   }
 };
